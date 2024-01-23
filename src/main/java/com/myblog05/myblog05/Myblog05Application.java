4f8510ff6308934cebdad58d8c504c3553f1d13d
zahid ansari <zahid776592@gmail.com>
2024-01-23 22:55:40 +0530

    127:create Save Post Feature

---
+++ src/main/java/com/myblog05/myblog05/Myblog05Application.java
@@ -0,0 +1,13 @@
+package com.myblog05.myblog05;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class Myblog05Application {
+
+	public static void main(String[] args) {
+		SpringApplication.run(Myblog05Application.class, args);
+	}
+
+}
